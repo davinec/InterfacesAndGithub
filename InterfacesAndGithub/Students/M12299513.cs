80e86f95d598f73580bbc77c0991621b475baefd
Eric Davin <davinec@mail.uc.edu>
2019-11-20 13:01:51 -0500

    Updated so that CompteLargestPrimeFactor is tested

--- InterfacesAndGithub/Students/M12299513.cs
+++ InterfacesAndGithub/Students/M12299513.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+* Interface Assignment
+* Eric Davin
+* davinec@mail.uc.edu
+* Contmporary Programing 
+* Impliment the interface
+* Due: 11/20/19 3:30
+* Citations: nicomp42's code
+*/
+
 namespace InterfacesAndGithub
 {
     class M12299513 : Student, IStudent
@@ -12,18 +22,41 @@ namespace InterfacesAndGithub
         {
             
         }
-
+        /// <summary>
+        /// Computes the largest Prime Factor is checked by the isPrime method
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
         public override long ComputeLargestPrimeFactor(long num)
         {
 
             long i = 2;
             while(i * i <= num)
             {
-                if (num % i == 0) num /= i;
+                if (num % i == 0 && isPrime(i)) num /= i;
 
                 else ++i;                                    
             }
             return num;
+        }
+        /// <summary>
+        /// Checks if a number is prime
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        private bool isPrime(long num)
+        {
+            
+            if (num <= 1) return false;
+
+            else
+            {   
+                for (int i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0) return false;
+                }
+                return true;
+            }
         }
     }
 }
