b8e7aa7320d588dfad0786357a549e7e21f578f5
Eric Davin <davinec@mail.uc.edu>
2019-11-20 12:36:45 -0500

    My MNumber class

--- InterfacesAndGithub/Students/M12299513.cs
+++ InterfacesAndGithub/Students/M12299513.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace InterfacesAndGithub
 {
-    class M12299513 : Student
+    class M12299513 : Student, IStudent
     {
+        public M12299513(string MNumber) : base(MNumber)
+        {
+            
+        }
+
+        public override long ComputeLargestPrimeFactor(long num)
+        {
+
+            long i = 2;
+            while(i * i <= num)
+            {
+                if (num % i == 0) num /= i;
+
+                else ++i;                                    
+            }
+            return num;
+        }
     }
 }
